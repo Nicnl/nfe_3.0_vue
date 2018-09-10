491a41b38cad5cd361f67f90f6d7b7153ab42ecd
Lim Nicolas <n.lim@pyres.com>
2018-09-10 14:07:59 +0200

    Amélioration tableau de listing des fichiers

--- src/views/ListFiles.vue
+++ src/views/ListFiles.vue
@@ -18,24 +18,22 @@
                 </tfoot>
                 <tbody>
                 <tr>
-                    <td>I</td>
-                    <td>debian.iso</td>
-                    <td>500 Mo</td>
+                    <td><i class="fal fa-folder"></i></td>
+                    <td colspan="2"><a href="http://google.fr">Windows</a></td>
                 </tr>
                 <tr>
-                    <td>I</td>
-                    <td>debian.iso</td>
-                    <td>500 Mo</td>
+                    <td><i class="fal fa-folder"></i></td>
+                    <td colspan="2"><a href="http://google.fr">Linux</a></td>
                 </tr>
                 <tr>
-                    <td>I</td>
-                    <td>debian.iso</td>
-                    <td>500 Mo</td>
+                    <td><i class="fal fa-file"></i></td>
+                    <td><a href="http://google.fr">README.md</a></td>
+                    <td>4.27 Ko</td>
                 </tr>
                 <tr>
-                    <td>I</td>
-                    <td>debian.iso</td>
-                    <td>500 Mo</td>
+                    <td><i class="fal fa-file"></i></td>
+                    <td><a href="http://google.fr">Dockerfile</a></td>
+                    <td>768 o</td>
                 </tr>
                 </tbody>
             </table>
@@ -45,6 +43,8 @@
 
 <style lang="scss">
     table.table {
+
+
         width: 100%;
         background-color: #f5f7fa;
 
@@ -52,12 +52,28 @@
             background-color: rgba(0, 0, 0, 0);
             transition: background-color 250ms;
 
+
             &:hover {
                 background-color: rgba(0, 0, 0, 0.05);
                 transition: background-color 100ms;
             }
 
-            cursor: pointer;
+            td {
+                padding-top: 0;
+                padding-bottom: 0;
+
+                $height: 37px;
+                height: $height;
+                line-height: $height;
+                vertical-align: middle;
+
+                a {
+                    color: inherit;
+                    display: block;
+                    text-decoration: none;
+                    cursor: pointer;
+                }
+            }
         }
     }
 </style>
